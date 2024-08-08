56d6c96394430e07ba231e269e4b7d4c68ca4e27
Kha <buitranminhkha79120@gmail.com>
2024-08-08 23:30:29 +0700

    Chuong2_Trang69_cau1_cau2_ Viết hàm nhập danh sách các phân số. Viết hàm xuất danh sách các phân số.

---
+++ KQandDebug_Chuong02/Chuong02/Chuong02/Chuong2_Trang69.cpp
@@ -0,0 +1,84 @@
+﻿#include <stdio.h>
+
+#define MAX_SIZE 50
+
+typedef struct {
+    int tu_so;
+    int mau_so;
+} PhanSo;
+
+// Hàm nhập danh sách các phân số
+void nhap_danh_sach(PhanSo arr[], int* n) {
+    printf("Nhap so luong phan so (toi da %d): ", MAX_SIZE);
+    scanf_s("%d", n);
+    if (*n > MAX_SIZE) {
+        printf("So luong phan so vuot qua gioi han, gan max = %d\n", MAX_SIZE);
+        *n = MAX_SIZE;
+    }
+
+    for (int i = 0; i < *n; ++i) {
+        printf("Nhap tu so cho phan so %d: ", i + 1);
+        scanf_s("%d", &arr[i].tu_so);
+        printf("Nhap mau so cho phan so %d: ", i + 1);
+        scanf_s("%d", &arr[i].mau_so);
+        while (arr[i].mau_so == 0) {
+            printf("Mau so phai khac 0. Vui long nhap lai mau so: ");
+            scanf_s("%d", &arr[i].mau_so);
+        }
+    }
+}
+
+// Hàm xuất danh sách các phân số
+void xuat_danh_sach(PhanSo arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        if (arr[i].mau_so == 1) {
+            printf("Phan so %d: %d\n", i + 1, arr[i].tu_so);
+        }
+        else {
+            printf("Phan so %d: %d/%d\n", i + 1, arr[i].tu_so, arr[i].mau_so);
+        }
+    }
+}
+
+
+
+void menu() {
+    PhanSo arr[MAX_SIZE];
+    int n, lua_chon;
+    PhanSo ket_qua;
+
+    do {
+        printf("\nMenu\n");
+        printf("1. Nhap danh sach phan so\n");
+        printf("2. Xuat danh sach phan so\n");
+        printf("3. Tim phan so lon nhat\n");
+        printf("4. Tim phan so nho nhat\n");
+        printf("5. Tinh tong cac phan so\n");
+        printf("6. Tinh tich cac phan so\n");
+        printf("7. Xuat nghich dao cac phan so\n");
+        printf("8. Sap xep tang dan\n");
+        printf("9. Sap xep giam dan\n");
+        printf("0. Thoat\n");
+        printf("Lua chon cua ban: ");
+        scanf_s("%d", &lua_chon);
+
+        switch (lua_chon) {
+        case 1:
+            nhap_danh_sach(arr, &n);
+            break;
+        case 2:
+            xuat_danh_sach(arr, n);
+            break;
+        case 0:
+            printf("Thoat chuong trinh.\n");
+            break;
+        default:
+            printf("Lua chon khong hop le!\n");
+        }
+    } while (lua_chon != 0);
+}
+
+int main() {
+    menu();
+    return 0;
+}
