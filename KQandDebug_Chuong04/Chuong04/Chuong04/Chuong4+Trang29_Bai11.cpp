630adc819e1f3277ea0cfe25dbe205bc40a5c6f2
Kha <buitranminhkha79120@gmail.com>
2024-08-09 00:34:17 +0700

    Chuong4_Trang29_Bai11 Fibonacci

---
+++ KQandDebug_Chuong04/Chuong04/Chuong04/Chuong4+Trang29_Bai11.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy để tính số Fibonacci tại vị trí thứ n
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// Hàm xuất dãy số Fibonacci có n phần tử
+void xuatDayFibonacci(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Nhap so luong phan tu cua day Fibonacci: ");
+    scanf_s("%d", &n);
+
+    printf("Day Fibonacci co %d so la: ", n);
+    xuatDayFibonacci(n);
+
+    return 0;
+}
