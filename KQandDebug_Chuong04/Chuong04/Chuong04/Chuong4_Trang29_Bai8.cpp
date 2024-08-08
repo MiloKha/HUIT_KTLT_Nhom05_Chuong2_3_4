f03ea0bc10ed13c6dd8925154aaad07ebfbf8ca8
Kha <buitranminhkha79120@gmail.com>
2024-08-09 00:30:36 +0700

    Chuong4_Trang29_Bai8_  Với n ≥ 1, n là số nguyên dương, biết rằng f(n) được tính theo công thức đệ quy như sau : f(1) = 1 ; f(2n) =2f(n) ; f(2n+1) = 2f(n) + 3f(n+1).

---
+++ KQandDebug_Chuong04/Chuong04/Chuong04/Chuong4_Trang29_Bai8.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy tính giá trị của f(n)
+int f(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else if (n % 2 == 0) {
+        return 2 * f(n / 2);
+    }
+    else {
+        return 2 * f(n / 2) + 3 * f(n / 2 + 1);
+    }
+}
+
+int main() {
+    int n, choice;
+    do {
+        printf("Menu:\n");
+        printf("1. Tinh f(5)\n");
+        printf("2. Nhap n va tinh f(n)\n");
+        printf("3. Thoat\n");
+        printf("Chon lua chon cua ban: ");
+        scanf_s("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("f(5) = %d\n", f(5));
+            break;
+        case 2:
+            printf("Nhap n: ");
+            scanf_s("%d", &n);
+            printf("f(%d) = %d\n", n, f(n));
+            break;
+        case 3:
+            printf("Thoat chuong trinh.\n");
+            break;
+        default:
+            printf("Lua chon khong hop le. Vui long chon lai.\n");
+            break;
+        }
+    } while (choice != 3);
+
+    return 0;
+}
